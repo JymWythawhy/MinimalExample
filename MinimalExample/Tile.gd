d773c0b5df76cf32c297179f2057b56710bba37e
JymWythawhy <djordanwalser@hotmail.com>
2016-11-21 21:36:16 -0500

    Add files via upload
    Changed the location of a print statement in on_Tile_input_event to only fire the print on a mouse click, as intended.

---
+++ MinimalExample/Tile.gd
@@ -0,0 +1,38 @@
+extends Control
+
+export (Texture) var Blank
+export (Texture) var Filled
+
+var filled = false
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+	
+
+func _on_Tile_input_event( ev ):
+	if(ev.type==InputEvent.MOUSE_BUTTON and ev.pressed):
+		if (ev.button_index==BUTTON_LEFT):
+			print("_on_Tile_mouse_click")
+			if(filled):
+				get_node("Sprite").set_texture(Blank)
+				filled = false
+			else:
+				get_node("Sprite").set_texture(Filled)
+				filled = true
+
+
+
+func _on_Tile_mouse_enter():
+	print("_on_Tile_mouse_enter")
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		if(filled):
+			get_node("Sprite").set_texture(Blank)
+			filled = false
+		else:
+			get_node("Sprite").set_texture(Filled)
+			filled = true
+
+
+
